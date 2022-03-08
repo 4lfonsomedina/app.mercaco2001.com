5691350f5d7e4ea34de7d2ace74431bd86915a04
Alfonso Medina <4lfonsomedina@gmail.com>
2022-03-07 19:23:39 -0800

    mi primer commit

---
+++ application/views/pie.php
@@ -0,0 +1,18 @@
+</div>
+<div class="notificacion" hidden></div>
+</body>
+<footer>
+	<script src="<?= base_url('assets/js/bootstrap.js') ?>"></script>
+	<script src="<?= base_url('assets/js/datatables.js') ?>"></script>
+	<script src="<?= base_url('assets/js/chosen.js') ?>"></script>
+	<script src="<?= base_url('assets/js/excel.js') ?>"></script>
+	<script src="<?= base_url('assets/js/funciones.js') ?>?v=5"></script>
+
+<?php 
+$url = explode("/", $_SERVER['REQUEST_URI']);
+if(end($url)=="Reportes_controller"){?>
+	<script src="<?= base_url('assets/js/reportes.js') ?>?v=1"></script>
+<?php } ?>
+	<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCNd6OjtDMttSHWKocaLW24wkAg6bp0peA&callback=construir_mapa"></script>
+</footer>	
+</html>
